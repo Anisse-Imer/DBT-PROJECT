313ccfb00d712b1d8f5af8bd373e88cbef43c1f4
Anisse-Imer <anissezou@gmail.com>
2024-10-22 10:44:37 +0200

    Passage folder - sub repository for Airflow :

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -5,6 +5,6 @@ select
 from
     {{ ref('stg_tpch_orders') }} as orders
 join
-    {{ ref('int_order_items_summary') }} as order_item_summary
-        on orders.order_key = order_item_summary.order_key
+    {{ ref('int_order_items_summary') }} as order_items_summary
+        on orders.order_key = order_items_summary.order_key
 order by order_date
